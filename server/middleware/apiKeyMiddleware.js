d87d48acfb450090638cca49748e97c5b4f89991
AfriLove Team <afriloveteam@gmail.com>
2026-03-11 20:59:41 +0100

    Make API_KEY middleware production-safe

--- server/middleware/apiKeyMiddleware.js
+++ server/middleware/apiKeyMiddleware.js
@@ -1,10 +1,12 @@
-require('dotenv').config();  // Loads environment variables
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 module.exports = (req, res, next) => {
-  const key = req.headers['x-api-key'];  // must match header sent
-  if (key && key === process.env.API_KEY) {
-    next();
-  } else {
-    res.status(401).json({ message: 'Unauthorized: Invalid API key' });
-  }
+    const key = req.headers['x-api-key'];
+    if (key && key === process.env.API_KEY) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Unauthorized: Invalid API key' });
+    }
 };
